efd87d7cfc54d8b87f5ed3853062ab694f80624d
abisek001 <abisek501@gmail.com>
2023-11-13 18:08:30 +0530

    home page created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Navbar from "./Components/Navbar/Navbar";
+import Home from "./Pages/Home/Home";
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div >
       <Navbar />
+      <Home />
     </div>
   );
 }
