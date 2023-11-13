678514bd9ae9f4389410c328b7f8dfdf47efe803
abisek001 <abisek501@gmail.com>
2023-11-13 18:35:01 +0530

    Home code page updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Navbar from "./Components/Navbar/Navbar";
 import Home from "./Pages/Home/Home";
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
