b03c401d4a6b5edc2146de2a7d282675a80fb1a4
abisek001 <abisek501@gmail.com>
2023-11-16 20:39:14 +0530

    leftside bar updated

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import './App.css';
 import Navbar from "./Components/Navbar/Navbar";
 import AllRoutes from './Components/AllRoutes';
 import { BrowserRouter } from "react-router-dom";
+import DrawerSidebar from './Components/Leftside_Bar/DrawerSidebar';
 
 function App() {
   return (
     <BrowserRouter >
       <Navbar />
+      {
+        <DrawerSidebar/>
+      }
       <AllRoutes />
     </BrowserRouter>
   );
---
+++ src/Components/Leftside_Bar/DrawerSidebar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function DrawerSidebar() {
+  return (
+    <div className="container_DrawerSidebar">
+      <div className="drawerSidebar_Items">
+        DrawerSidebar
+        </div>
+    </div>
+  );
+}
+
+export default DrawerSidebar;
