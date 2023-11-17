fe03779f8d9e953307da9dd552d929d4c19deff9
abisek001 <abisek501@gmail.com>
2023-11-17 12:54:49 +0530

    left side bar updated

--- src/Components/Leftside_Bar/DrawerSidebar.jsx
+++ src/Components/Leftside_Bar/DrawerSidebar.jsx
@@ -1,11 +1,52 @@
 import React from "react";
+import { AiFillPlaySquare, AiOutlineHome } from "react-icons/ai";
+import { FaHistory } from "react-icons/fa";
+import {
+  MdOutlineSubscriptions,
+  MdOutlineVideoLibrary,
+  MdOutlineExplore,
+} from "react-icons/md";
 
 function DrawerSidebar() {
   return (
     <div className="container_DrawerSidebar">
       <div className="drawerSidebar_Items">
-        DrawerSidebar
+        <div className="drawer_Icone">
+          <AiOutlineHome size={22} />
+          <span className="drawer_HomeIconeText">Home</span>
         </div>
+        <div className="drawer_Icone">
+          <MdOutlineExplore size={22} />
+          <span className="drawer_HomeIconeText">Explore</span>
+        </div>
+        <div className="drawer_Icone">
+          <img
+            width="24"
+            height="24"
+            src="https://img.icons8.com/material-outlined/24/youtube-shorts.png"
+            alt="youtube-shorts"
+          />
+          <span className="drawer_HomeIconeText">Shorts</span>
+        </div>
+        <div className="drawer_Icone">
+          <MdOutlineSubscriptions size={22} />
+          <span className="drawer_HomeIconeText">Subscriptions</span>
+        </div>
+        <div className="library_Drawer_Sidebar">
+          <div className="drawer_Icone">
+            <MdOutlineVideoLibrary size={22} />
+            <span className="drawer_HomeIconeText">Library</span>
+          </div>
+          <div className="drawer_Icone">
+            <FaHistory size={22} />
+            <span className="drawer_HomeIconeText">History</span>
+          </div>
+          <div className="drawer_Icone">
+            <AiFillPlaySquare size={22} />
+            <span className="drawer_HomeIconeText">Your Video</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/Components/Leftside_Bar/LeftsideBar.jsx
+++ src/Components/Leftside_Bar/LeftsideBar.jsx
@@ -1,19 +1,18 @@
 import React from "react";
-import { MdOutlineSubscriptions, MdOutlineVideoLibrary, MdOutlineExplore } from "react-icons/md";
+import {
+  MdOutlineSubscriptions,
+  MdOutlineVideoLibrary,
+  MdOutlineExplore,
+} from "react-icons/md";
 import "./LeftsideBar.css";
 import { NavLink } from "react-router-dom";
+import { AiOutlineHome } from "react-icons/ai";
 
 function LeftsideBar() {
   return (
     <div className="LeftsideBar_Container">
-      <NavLink to = {'/'} className="icones_Container">
-        <img
-          width="24"
-          height="24"
-          src="https://img.icons8.com/material-sharp/24/home.png"
-          alt="home"
-          className="homeIcone"
-        />
+      <NavLink to={"/"} className="icones_Container">
+        <AiOutlineHome className="homeIcone" size={22} />
         <span className="homeIconeText">Home</span>
       </NavLink>
       <div className="icones_Container">
@@ -21,10 +20,20 @@ function LeftsideBar() {
         <span className="homeIconeText">Explore</span>
       </div>
       <div className="icones_Container">
+      <img
+            width="24"
+            height="24"
+            src="https://img.icons8.com/material-outlined/24/youtube-shorts.png"
+            alt="youtube-shorts"
+            className="homeIcone"
+          />
+        <span className="homeIconeText">Shorts</span>
+      </div>
+      <div className="icones_Container">
         <MdOutlineSubscriptions size={22} className="homeIcone" />
         <span className="homeIconeText">Subscription</span>
       </div>
-      <NavLink to={'/library'} className="icones_Container">
+      <NavLink to={"/library"} className="icones_Container">
         <MdOutlineVideoLibrary size={22} className="homeIcone" />
         <span className="homeIconeText">Library</span>
       </NavLink>
