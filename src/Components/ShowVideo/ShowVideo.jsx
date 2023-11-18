20bb516375253da1577836e5a2e9664c6a283896
abisek001 <abisek501@gmail.com>
2023-11-18 15:19:12 +0530

    video list and video details created

---
+++ src/Components/ShowVideo/ShowVideo.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "./ShowVideo.css";
+
+function ShowVideo({ vids }) {
+  return (
+    <>
+      <div>
+        <video controls className="Video_player">
+          <source src={`${vids?.video_src}`} />
+        </video>
+      </div>
+      <div className="video_Description">
+        {vids.logo === undefined || null ? (
+          <div className="chanel_Video">
+            <div className="char_Video_logo">
+              <span className="chanel_Video_logo">
+                {" "}
+                {vids?.Uploder?.charAt(0).toUpperCase()}
+              </span>
+            </div>
+          </div>
+        ) : (
+          <div className="chanel_Video">
+            <div className="char_Video_logo">
+              <img
+                src={`${vids?.logo}`}
+                alt="chanel logo"
+                className="chanel_Video_logo"
+              />
+            </div>
+          </div>
+        )}
+        <div className="video_Details">
+          <span className="video_Title">{vids?.title}</span>
+          <br/>
+          <span className="Chanel_Name">{vids?.Chanel}</span> 
+
+          <span className="video_Views">1k views<div className="dot">.</div> 1years ago</span>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ShowVideo;
---
+++ src/Components/ShowVideoGrid/ShowVideoGrid.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ShowVideo from "../ShowVideo/ShowVideo";
+import './ShowVideoGrid.css';
+
+function ShowVideoGrid({ videos }) {
+  return (
+    <div className="video_Grid_Container">
+      {videos.map((vid) => {
+        return <div className="video_Box_App" key={vid._id}>
+            <ShowVideo vids = {vid}/>
+        </div>;
+      })}
+    </div>
+  );
+}
+
+export default ShowVideoGrid;
