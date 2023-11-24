e873f7b39a09e0ddcbb91601ab0aed596fca57be
abisek001 <abisek501@gmail.com>
2023-11-24 19:06:24 +0530

    top navbar suggestion list created

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+
 import LeftsideBar from "../../Components/Leftside_Bar/LeftsideBar";
 import ShowVideoGrid from "../../Components/ShowVideoGrid/ShowVideoGrid";
+import './Home.css';
 
 function Home() {
   const videos = [
@@ -89,12 +91,130 @@ function Home() {
       /* logo:"https://img.icons8.com/fluency/48/marvel.png", */
       description: "description of video 4",
     },
-    
+    {
+      _id: 1,
+      video_src:
+        "https://res.cloudinary.com/ehizeex-shop/video/upload/v1668377666/NetflixApp/Action_mlw9wx.mp4",
+      Chanel: "Marvel India",
+      logo: "https://img.icons8.com/avantgarde/100/avengers.png",
+      Uploder: "Srikanth",
+      title: "Marvel Studios' The Marvels | Official Trailer",
+      description: "description of video 1",
+    },
+    {
+      _id: 2,
+      video_src:
+        "https://res.cloudinary.com/ehizeex-shop/video/upload/v1668377666/NetflixApp/Action_mlw9wx.mp4",
+      Chanel: "Marvel Artist",
+      /* logo:"https://img.icons8.com/avantgarde/100/avengers.png", */
+      Uploder: "Abisek",
+      title: "Madame Web | Official Trailer",
+      description: "description of video 2",
+    },
+    {
+      _id: 3,
+      video_src:
+        "https://res.cloudinary.com/ehizeex-shop/video/upload/v1668377666/NetflixApp/Action_mlw9wx.mp4",
+      Chanel: "Marvel Entertinment",
+      title: "Top 10 Superheroes Too Dark For The MCU",
+      Uploder: "Ranjith",
+      logo: "https://img.icons8.com/fluency/48/marvel.png",
+      description: "description of video 3",
+    },
+    {
+      _id: 4,
+      video_src:
+        "https://res.cloudinary.com/ehizeex-shop/video/upload/v1668377666/NetflixApp/Action_mlw9wx.mp4",
+      Chanel: "Marvel Edit",
+      title:
+        "Marvel Studios' What If...? Season 2 | Official Trailer In Hindi (AI) | Disney+",
+      Uploder: "Naveen",
+      /* logo:"https://img.icons8.com/fluency/48/marvel.png", */
+      description: "description of video 4",
+    },
+    {
+      _id: 5,
+      video_src:
+        "https://res.cloudinary.com/ehizeex-shop/video/upload/v1668377666/NetflixApp/Action_mlw9wx.mp4",
+      Chanel: "Marvel Edit",
+      title:
+        "Marvel Studios' What If...? Season 2 | Official Trailer In Hindi (AI) | Disney+",
+      Uploder: "Naveen",
+      /* logo:"https://img.icons8.com/fluency/48/marvel.png", */
+      description: "description of video 4",
+    },
+    {
+      _id: 5,
+      video_src:
+        "https://res.cloudinary.com/ehizeex-shop/video/upload/v1668377666/NetflixApp/Action_mlw9wx.mp4",
+      Chanel: "Marvel Edit",
+      title:
+        "Marvel Studios' What If...? Season 2 | Official Trailer In Hindi (AI) | Disney+",
+      Uploder: "Naveen",
+      /* logo:"https://img.icons8.com/fluency/48/marvel.png", */
+      description: "description of video 4",
+    },
+    {
+      _id: 5,
+      video_src:
+        "https://res.cloudinary.com/ehizeex-shop/video/upload/v1668377666/NetflixApp/Action_mlw9wx.mp4",
+      Chanel: "Marvel Edit",
+      title:
+        "Marvel Studios' What If...? Season 2 | Official Trailer In Hindi (AI) | Disney+",
+      Uploder: "Naveen",
+      /* logo:"https://img.icons8.com/fluency/48/marvel.png", */
+      description: "description of video 4",
+    },
+    {
+      _id: 5,
+      video_src:
+        "https://res.cloudinary.com/ehizeex-shop/video/upload/v1668377666/NetflixApp/Action_mlw9wx.mp4",
+      Chanel: "Marvel Edit",
+      title:
+        "Marvel Studios' What If...? Season 2 | Official Trailer In Hindi (AI) | Disney+",
+      Uploder: "Naveen",
+      /* logo:"https://img.icons8.com/fluency/48/marvel.png", */
+      description: "description of video 4",
+    },
+  ];
+
+  const NavList = [
+    "All",
+    "Java",
+    "HTML",
+    "CSS",
+    "Javascript",
+    "Python",
+    "Bootstrap",
+    "React",
+    "Node",
+    "Express",
+    "Github",
+    "Data",
+    "Movie",
+    "Songs",
+    "Motivation",
+    "Programming",
+    "Comedy",
+    "Movie",
+    "Songs",
+    "Motivation",
+    "Programming",
+    "Comedy"
   ];
   return (
     <div className="app_Page_Container">
       <LeftsideBar />
       <div className="app_Page_Items">
+        <div className="navigation_List">
+          {NavList.map((item) => {
+            return (
+              <p className="navList_Button" key={item}>
+                {item}
+              </p>
+            );
+          })}
+        </div>
         <ShowVideoGrid videos={videos} />
       </div>
     </div>
