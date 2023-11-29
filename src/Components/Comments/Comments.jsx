a441e24c8f77c2d8b2807c42830cf311d456bfac
abisek001 <abisek501@gmail.com>
2023-11-29 08:09:04 +0530

    video comments updated

--- src/Components/Comments/Comments.jsx
+++ src/Components/Comments/Comments.jsx
@@ -1,19 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Comments.css";
+import DisplayComments from "./DisplayComments";
 
 function Comments() {
+  const [comment, setComment] = useState("");
+
+  const handleCommentChange = (e) => {
+    setComment(e.target.value);
+  };
+
+  const handleCancel = () => {
+    setComment("");
+  };
+
+  const handleSubmmit = (i) => {
+    i.preventDefault();
+    setComment("");
+  };
+
   return (
     <div className="comments_Container">
-      <form className="comments_Form">
-        <input
-          type="text"
-          placeholder="Add a comment..."
-          className="comments_Input"
-        />
-      </form>
-      <div className="buttons_Container">
-        <input type="button" value="Cancel" className="cancel_Button" />
-        <input type="submit" value="Comment" className="comment_Button" />
+      <div className="comment_Field_Continer">
+        <form className="comments_Form">
+          <input
+            type="text"
+            placeholder="Add a comment..."
+            className="comments_Input"
+            value={comment}
+            onChange={handleCommentChange}
+          />
+        </form>
+        <div className="buttons_Container">
+          <input
+            type="button"
+            value="Cancel"
+            className="cancel_Button"
+            onClick={handleCancel}
+          />
+          <input
+            type="submit"
+            value="Comment"
+            className="comment_Button"
+            onClick={handleSubmmit}
+          />
+        </div>
+      </div>
+      <div className="display_Comments_Container">
+        <DisplayComments/>
       </div>
     </div>
   );
---
+++ src/Components/Comments/DisplayComments.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function DisplayComments() {
+  return (
+    <div>
+        dc
+    </div>
+  )
+}
+
+export default DisplayComments;
