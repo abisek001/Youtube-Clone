1cc26dc6445e0affe5fc4d4e5d5b8389a2321152
abisek001 <abisek501@gmail.com>
2023-12-05 20:37:14 +0530

    your video page and library page updated

--- src/Pages/Library/Library.jsx
+++ src/Pages/Library/Library.jsx
@@ -1,12 +1,188 @@
 import React from 'react'
+
 import LeftsideBar from '../../Components/Leftside_Bar/LeftsideBar';
+import './Library.css';
 
 function Library() {
+  const videos = [
+    {
+      _id: 1,
+      video_src:
+        "https://res.cloudinary.com/ehizeex-shop/video/upload/v1668377666/NetflixApp/Action_mlw9wx.mp4",
+      Chanel: "Marvel India",
+      logo: "https://img.icons8.com/avantgarde/100/avengers.png",
+      Uploder: "Srikanth",
+      title: "Marvel Studios' The Marvels | Official Trailer",
+      description: "description of video 1",
+    },
+    {
+      _id: 2,
+      video_src:
+        "https://res.cloudinary.com/ehizeex-shop/video/upload/v1668377666/NetflixApp/Action_mlw9wx.mp4",
+      Chanel: "Marvel Artist",
+      /* logo:"https://img.icons8.com/avantgarde/100/avengers.png", */
+      Uploder: "Abisek",
+      title: "Madame Web | Official Trailer",
+      description: "description of video 2",
+    },
+    {
+      _id: 3,
+      video_src:
+        "https://res.cloudinary.com/ehizeex-shop/video/upload/v1668377666/NetflixApp/Action_mlw9wx.mp4",
+      Chanel: "Marvel Entertinment",
+      title: "Top 10 Superheroes Too Dark For The MCU",
+      Uploder: "Ranjith",
+      logo: "https://img.icons8.com/fluency/48/marvel.png",
+      description: "description of video 3",
+    },
+    {
+      _id: 4,
+      video_src:
+        "https://res.cloudinary.com/ehizeex-shop/video/upload/v1668377666/NetflixApp/Action_mlw9wx.mp4",
+      Chanel: "Marvel Edit",
+      title:
+        "Marvel Studios' What If...? Season 2 | Official Trailer In Hindi (AI) | Disney+",
+      Uploder: "Naveen",
+      /* logo:"https://img.icons8.com/fluency/48/marvel.png", */
+      description: "description of video 4",
+    },
+    {
+      _id: 5,
+      video_src:
+        "https://res.cloudinary.com/ehizeex-shop/video/upload/v1668377666/NetflixApp/Action_mlw9wx.mp4",
+      Chanel: "Marvel Edit",
+      title:
+        "Marvel Studios' What If...? Season 2 | Official Trailer In Hindi (AI) | Disney+",
+      Uploder: "Naveen",
+      /* logo:"https://img.icons8.com/fluency/48/marvel.png", */
+      description: "description of video 4",
+    },
+    {
+      _id: 5,
+      video_src:
+        "https://res.cloudinary.com/ehizeex-shop/video/upload/v1668377666/NetflixApp/Action_mlw9wx.mp4",
+      Chanel: "Marvel Edit",
+      title:
+        "Marvel Studios' What If...? Season 2 | Official Trailer In Hindi (AI) | Disney+",
+      Uploder: "Naveen",
+      /* logo:"https://img.icons8.com/fluency/48/marvel.png", */
+      description: "description of video 4",
+    },
+    {
+      _id: 5,
+      video_src:
+        "https://res.cloudinary.com/ehizeex-shop/video/upload/v1668377666/NetflixApp/Action_mlw9wx.mp4",
+      Chanel: "Marvel Edit",
+      title:
+        "Marvel Studios' What If...? Season 2 | Official Trailer In Hindi (AI) | Disney+",
+      Uploder: "Naveen",
+      /* logo:"https://img.icons8.com/fluency/48/marvel.png", */
+      description: "description of video 4",
+    },
+    {
+      _id: 5,
+      video_src:
+        "https://res.cloudinary.com/ehizeex-shop/video/upload/v1668377666/NetflixApp/Action_mlw9wx.mp4",
+      Chanel: "Marvel Edit",
+      title:
+        "Marvel Studios' What If...? Season 2 | Official Trailer In Hindi (AI) | Disney+",
+      Uploder: "Naveen",
+      /* logo:"https://img.icons8.com/fluency/48/marvel.png", */
+      description: "description of video 4",
+    },
+    {
+      _id: 1,
+      video_src:
+        "https://res.cloudinary.com/ehizeex-shop/video/upload/v1668377666/NetflixApp/Action_mlw9wx.mp4",
+      Chanel: "Marvel India",
+      logo: "https://img.icons8.com/avantgarde/100/avengers.png",
+      Uploder: "Srikanth",
+      title: "Marvel Studios' The Marvels | Official Trailer",
+      description: "description of video 1",
+    },
+    {
+      _id: 2,
+      video_src:
+        "https://res.cloudinary.com/ehizeex-shop/video/upload/v1668377666/NetflixApp/Action_mlw9wx.mp4",
+      Chanel: "Marvel Artist",
+      /* logo:"https://img.icons8.com/avantgarde/100/avengers.png", */
+      Uploder: "Abisek",
+      title: "Madame Web | Official Trailer",
+      description: "description of video 2",
+    },
+    {
+      _id: 3,
+      video_src:
+        "https://res.cloudinary.com/ehizeex-shop/video/upload/v1668377666/NetflixApp/Action_mlw9wx.mp4",
+      Chanel: "Marvel Entertinment",
+      title: "Top 10 Superheroes Too Dark For The MCU",
+      Uploder: "Ranjith",
+      logo: "https://img.icons8.com/fluency/48/marvel.png",
+      description: "description of video 3",
+    },
+    {
+      _id: 4,
+      video_src:
+        "https://res.cloudinary.com/ehizeex-shop/video/upload/v1668377666/NetflixApp/Action_mlw9wx.mp4",
+      Chanel: "Marvel Edit",
+      title:
+        "Marvel Studios' What If...? Season 2 | Official Trailer In Hindi (AI) | Disney+",
+      Uploder: "Naveen",
+      /* logo:"https://img.icons8.com/fluency/48/marvel.png", */
+      description: "description of video 4",
+    },
+    {
+      _id: 5,
+      video_src:
+        "https://res.cloudinary.com/ehizeex-shop/video/upload/v1668377666/NetflixApp/Action_mlw9wx.mp4",
+      Chanel: "Marvel Edit",
+      title:
+        "Marvel Studios' What If...? Season 2 | Official Trailer In Hindi (AI) | Disney+",
+      Uploder: "Naveen",
+      /* logo:"https://img.icons8.com/fluency/48/marvel.png", */
+      description: "description of video 4",
+    },
+    {
+      _id: 5,
+      video_src:
+        "https://res.cloudinary.com/ehizeex-shop/video/upload/v1668377666/NetflixApp/Action_mlw9wx.mp4",
+      Chanel: "Marvel Edit",
+      title:
+        "Marvel Studios' What If...? Season 2 | Official Trailer In Hindi (AI) | Disney+",
+      Uploder: "Naveen",
+      /* logo:"https://img.icons8.com/fluency/48/marvel.png", */
+      description: "description of video 4",
+    },
+    {
+      _id: 5,
+      video_src:
+        "https://res.cloudinary.com/ehizeex-shop/video/upload/v1668377666/NetflixApp/Action_mlw9wx.mp4",
+      Chanel: "Marvel Edit",
+      title:
+        "Marvel Studios' What If...? Season 2 | Official Trailer In Hindi (AI) | Disney+",
+      Uploder: "Naveen",
+      /* logo:"https://img.icons8.com/fluency/48/marvel.png", */
+      description: "description of video 4",
+    },
+    {
+      _id: 5,
+      video_src:
+        "https://res.cloudinary.com/ehizeex-shop/video/upload/v1668377666/NetflixApp/Action_mlw9wx.mp4",
+      Chanel: "Marvel Edit",
+      title:
+        "Marvel Studios' What If...? Season 2 | Official Trailer In Hindi (AI) | Disney+",
+      Uploder: "Naveen",
+      /* logo:"https://img.icons8.com/fluency/48/marvel.png", */
+      description: "description of video 4",
+    },
+  ];
   return (
     <div className='app_Page_Container'>
       <LeftsideBar/>
         <div className='app_Page_Items'>
-            Library
+            <div className='videoPage_Container'>
+
+            </div>
         </div>
     </div>
   )
--- src/Pages/YourVideo/YourVideo.jsx
+++ src/Pages/YourVideo/YourVideo.jsx
@@ -1,9 +1,194 @@
-import React from 'react'
+import React from "react";
+
+import LeftsideBar from "../../Components/Leftside_Bar/LeftsideBar";
+import ShowVideoGrid from "../../Components/ShowVideoGrid/ShowVideoGrid";
+import "./yourVideo.css";
 
 function YourVideo() {
+  const videos = [
+    {
+      _id: 1,
+      video_src:
+        "https://res.cloudinary.com/ehizeex-shop/video/upload/v1668377666/NetflixApp/Action_mlw9wx.mp4",
+      Chanel: "Marvel India",
+      logo: "https://img.icons8.com/avantgarde/100/avengers.png",
+      Uploder: "Srikanth",
+      title: "Marvel Studios' The Marvels | Official Trailer",
+      description: "description of video 1",
+    },
+    {
+      _id: 2,
+      video_src:
+        "https://res.cloudinary.com/ehizeex-shop/video/upload/v1668377666/NetflixApp/Action_mlw9wx.mp4",
+      Chanel: "Marvel Artist",
+      /* logo:"https://img.icons8.com/avantgarde/100/avengers.png", */
+      Uploder: "Abisek",
+      title: "Madame Web | Official Trailer",
+      description: "description of video 2",
+    },
+    {
+      _id: 3,
+      video_src:
+        "https://res.cloudinary.com/ehizeex-shop/video/upload/v1668377666/NetflixApp/Action_mlw9wx.mp4",
+      Chanel: "Marvel Entertinment",
+      title: "Top 10 Superheroes Too Dark For The MCU",
+      Uploder: "Ranjith",
+      logo: "https://img.icons8.com/fluency/48/marvel.png",
+      description: "description of video 3",
+    },
+    {
+      _id: 4,
+      video_src:
+        "https://res.cloudinary.com/ehizeex-shop/video/upload/v1668377666/NetflixApp/Action_mlw9wx.mp4",
+      Chanel: "Marvel Edit",
+      title:
+        "Marvel Studios' What If...? Season 2 | Official Trailer In Hindi (AI) | Disney+",
+      Uploder: "Naveen",
+      /* logo:"https://img.icons8.com/fluency/48/marvel.png", */
+      description: "description of video 4",
+    },
+    {
+      _id: 5,
+      video_src:
+        "https://res.cloudinary.com/ehizeex-shop/video/upload/v1668377666/NetflixApp/Action_mlw9wx.mp4",
+      Chanel: "Marvel Edit",
+      title:
+        "Marvel Studios' What If...? Season 2 | Official Trailer In Hindi (AI) | Disney+",
+      Uploder: "Naveen",
+      /* logo:"https://img.icons8.com/fluency/48/marvel.png", */
+      description: "description of video 4",
+    },
+    {
+      _id: 5,
+      video_src:
+        "https://res.cloudinary.com/ehizeex-shop/video/upload/v1668377666/NetflixApp/Action_mlw9wx.mp4",
+      Chanel: "Marvel Edit",
+      title:
+        "Marvel Studios' What If...? Season 2 | Official Trailer In Hindi (AI) | Disney+",
+      Uploder: "Naveen",
+      /* logo:"https://img.icons8.com/fluency/48/marvel.png", */
+      description: "description of video 4",
+    },
+    {
+      _id: 5,
+      video_src:
+        "https://res.cloudinary.com/ehizeex-shop/video/upload/v1668377666/NetflixApp/Action_mlw9wx.mp4",
+      Chanel: "Marvel Edit",
+      title:
+        "Marvel Studios' What If...? Season 2 | Official Trailer In Hindi (AI) | Disney+",
+      Uploder: "Naveen",
+      /* logo:"https://img.icons8.com/fluency/48/marvel.png", */
+      description: "description of video 4",
+    },
+    {
+      _id: 5,
+      video_src:
+        "https://res.cloudinary.com/ehizeex-shop/video/upload/v1668377666/NetflixApp/Action_mlw9wx.mp4",
+      Chanel: "Marvel Edit",
+      title:
+        "Marvel Studios' What If...? Season 2 | Official Trailer In Hindi (AI) | Disney+",
+      Uploder: "Naveen",
+      /* logo:"https://img.icons8.com/fluency/48/marvel.png", */
+      description: "description of video 4",
+    },
+    {
+      _id: 1,
+      video_src:
+        "https://res.cloudinary.com/ehizeex-shop/video/upload/v1668377666/NetflixApp/Action_mlw9wx.mp4",
+      Chanel: "Marvel India",
+      logo: "https://img.icons8.com/avantgarde/100/avengers.png",
+      Uploder: "Srikanth",
+      title: "Marvel Studios' The Marvels | Official Trailer",
+      description: "description of video 1",
+    },
+    {
+      _id: 2,
+      video_src:
+        "https://res.cloudinary.com/ehizeex-shop/video/upload/v1668377666/NetflixApp/Action_mlw9wx.mp4",
+      Chanel: "Marvel Artist",
+      /* logo:"https://img.icons8.com/avantgarde/100/avengers.png", */
+      Uploder: "Abisek",
+      title: "Madame Web | Official Trailer",
+      description: "description of video 2",
+    },
+    {
+      _id: 3,
+      video_src:
+        "https://res.cloudinary.com/ehizeex-shop/video/upload/v1668377666/NetflixApp/Action_mlw9wx.mp4",
+      Chanel: "Marvel Entertinment",
+      title: "Top 10 Superheroes Too Dark For The MCU",
+      Uploder: "Ranjith",
+      logo: "https://img.icons8.com/fluency/48/marvel.png",
+      description: "description of video 3",
+    },
+    {
+      _id: 4,
+      video_src:
+        "https://res.cloudinary.com/ehizeex-shop/video/upload/v1668377666/NetflixApp/Action_mlw9wx.mp4",
+      Chanel: "Marvel Edit",
+      title:
+        "Marvel Studios' What If...? Season 2 | Official Trailer In Hindi (AI) | Disney+",
+      Uploder: "Naveen",
+      /* logo:"https://img.icons8.com/fluency/48/marvel.png", */
+      description: "description of video 4",
+    },
+    {
+      _id: 5,
+      video_src:
+        "https://res.cloudinary.com/ehizeex-shop/video/upload/v1668377666/NetflixApp/Action_mlw9wx.mp4",
+      Chanel: "Marvel Edit",
+      title:
+        "Marvel Studios' What If...? Season 2 | Official Trailer In Hindi (AI) | Disney+",
+      Uploder: "Naveen",
+      /* logo:"https://img.icons8.com/fluency/48/marvel.png", */
+      description: "description of video 4",
+    },
+    {
+      _id: 5,
+      video_src:
+        "https://res.cloudinary.com/ehizeex-shop/video/upload/v1668377666/NetflixApp/Action_mlw9wx.mp4",
+      Chanel: "Marvel Edit",
+      title:
+        "Marvel Studios' What If...? Season 2 | Official Trailer In Hindi (AI) | Disney+",
+      Uploder: "Naveen",
+      /* logo:"https://img.icons8.com/fluency/48/marvel.png", */
+      description: "description of video 4",
+    },
+    {
+      _id: 5,
+      video_src:
+        "https://res.cloudinary.com/ehizeex-shop/video/upload/v1668377666/NetflixApp/Action_mlw9wx.mp4",
+      Chanel: "Marvel Edit",
+      title:
+        "Marvel Studios' What If...? Season 2 | Official Trailer In Hindi (AI) | Disney+",
+      Uploder: "Naveen",
+      /* logo:"https://img.icons8.com/fluency/48/marvel.png", */
+      description: "description of video 4",
+    },
+    {
+      _id: 5,
+      video_src:
+        "https://res.cloudinary.com/ehizeex-shop/video/upload/v1668377666/NetflixApp/Action_mlw9wx.mp4",
+      Chanel: "Marvel Edit",
+      title:
+        "Marvel Studios' What If...? Season 2 | Official Trailer In Hindi (AI) | Disney+",
+      Uploder: "Naveen",
+      /* logo:"https://img.icons8.com/fluency/48/marvel.png", */
+      description: "description of video 4",
+    },
+  ];
+
   return (
-    <div>YourVideo</div>
-  )
+    <div className="app_Page_Container">
+      <LeftsideBar />
+      <div className="app_Page_Items">
+        <div className="yourVideo_Container">
+          <h1>Your Video</h1>
+          <ShowVideoGrid videos={videos} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default YourVideo
+export default YourVideo;
