f99e62074a4bc81efae9e698d1e27c2decccadef
abisek001 <abisek501@gmail.com>
2023-12-09 05:07:28 +0530

    server and client connection and need to fix errors

---
+++ src/API/index.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+const API = create({serverURL: 'http://localhost:6845/'})
+API.interceptors.request.use(req => {
+    if (localStorage.getItem('profile')) {
+        req.headers.authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`
+    } 
+    return req;
+})
--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { thunk } from 'redux-thunk';
+import { applyMiddleware, compose } from 'redux';
+import {createStore} from 'redux';
+import { Reducers } from './Reducers';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+const store = createStore(Reducers, compose(applyMiddleware(thunk)))
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store = { store }>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
 
 reportWebVitals();
